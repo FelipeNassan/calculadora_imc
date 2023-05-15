e17530e4aa8576f71840feb0d1f5fd2b4f6130a6
Felipe do Nascimento Santos <felipenasantos2004@gmail.com>
2023-05-15 16:48:20 -0300

    Atualização do sistema com a criação de funções para realizar as funcionalidades.

--- calculadora_imc.dart
+++ calculadora_imc.dart
@@ -2,32 +2,59 @@ import 'dart:io';
 import 'dart:math';
 
 main() {
-  // Calculadora de IMC - Índice de Massa Corporal 
+  // Calculadora de IMC - Índice de Massa Corporal
   print("\n<> CALCULADORA DE IMC <>");
 
   // Coleta dos dados
   print("Insira as seguintes informações: ");
-  stdout.write("[ ALTURA ] ");
-  var dAltura = double.parse(stdin.readLineSync().toString());
-  stdout.write("[ PESO ] ");
-  var dPeso = double.parse(stdin.readLineSync().toString());
+  double dAlturaPaciente = pegarAltura();
+  double dPesoPaciente = pegarPeso();
 
-  // Cálculo do IMC (Peso / Altura ^ 2)
-  final calculoIMC = dPeso / pow(dAltura, 2);
-  print("\n[ IMC ] $calculoIMC");
-
+  // Retorno do valor do IMC e o respectivo status
+  double dValorIMC = calculoIMC(dPesoPaciente, dAlturaPaciente);
+  stdout.write('\n[ IMC ] $dValorIMC\n[ CLASSIFICAÇÃO ] ');
+  
   // Classificação do IMC
-  if (calculoIMC < 18.5) {
-    print("[ CLASSIFICAÇAÕ ] Abaixo do peso\n");
-  } else if (calculoIMC >= 18.5 && calculoIMC <= 24.9) {
-    print("[ CLASSIFICAÇAÕ ] Peso normal\n");
-  } else if (calculoIMC >= 25 && calculoIMC <= 29.9) {
-    print("[ CLASSIFICAÇAÕ ] Sobrepeso\n");
-  } else if (calculoIMC >= 30 && calculoIMC <= 34.9) {
-    print("[ CLASSIFICAÇAÕ ] Obesidade grau I\n");
-  } else if (calculoIMC >= 35 && calculoIMC <= 39.9) {
-    print("[ CLASSIFICAÇAÕ ] Obesidade grau II\n");
+  if (dValorIMC < 18.5) {
+    print('Abaixo do peso');
+  } else if (dValorIMC >= 18.5 && dValorIMC <= 24.9) {
+    print('Peso normal');
+  } else if (dValorIMC >= 25 && dValorIMC <= 29.9) {
+    print('Sobrepeso');
+  } else if (dValorIMC >= 30 && dValorIMC <= 34.9) {
+    print('Obesidade grau I');
+  } else if (dValorIMC >= 35 && dValorIMC <= 39.9) {
+    print('Obesidade grau II');
   } else {
-    print("[ CLASSIFICAÇAÕ ] Obesidade grau III ou Mórbida\n");
+    print('Obesidade grau III ou Mórbida');
   }
+  stdout.write('\n');
+}
+
+/// Função para pegar a altura de uma pessoa.
+double pegarAltura() {
+  stdout.write("[ ALTURA ] ");
+  final altura = stdin.readLineSync();
+  if (altura == null) {
+    return 0.0;
+  } else {
+    return double.parse(altura);
+  }
+}
+
+/// Função para pegar o peso de uma pessoa.
+double pegarPeso() {
+  stdout.write("[ PESO ] ");
+  final peso = stdin.readLineSync();
+  if (peso == null) {
+    return 0.0;
+  } else {
+    return double.parse(peso);
+  }
+}
+
+/// Função que realiza o cálculo do IMC (Peso / Altura ^ 2)
+double calculoIMC(double dPeso, double dAltura) {
+  final IMC = dPeso / pow(dAltura, 2);
+  return IMC;
 }
